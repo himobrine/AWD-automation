a2373e42debb4fb821a67cac1ed9cde727a45b24
himobrine <153483477+himobrine@users.noreply.github.com>
2024-08-23 21:31:51 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36'
-}
-url ='http://192.168.21.186/'
-r = requests.get(url, headers=headers)
-print(r.text)
