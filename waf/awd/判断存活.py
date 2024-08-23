b9b38cca9c2eebfcb0de97f2ab04ee1ea979bffb
himobrine <153483477+himobrine@users.noreply.github.com>
2024-08-23 21:37:29 +0800

    Add files via upload

---
+++ waf/awd/判断存活.py
@@ -0,0 +1,24 @@
+import asyncio
+import aiohttp
+
+async def check_ip(ip):
+    url = f"http://{ip}"
+    try:
+        async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=1)) as session:
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    print(ip)
+    except:
+        pass
+
+async def main():
+    tasks = []
+    # 构造要扫描的 IP 列表
+    ips = [f"185.203.239.{i}" for i in range(2, 255)]
+    for ip in ips:
+        tasks.append(asyncio.create_task(check_ip(ip)))
+
+    # 等待所有任务完成
+    await asyncio.gather(*tasks)
+
+asyncio.run(main())
